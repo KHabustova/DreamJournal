5a9b2bdb668277b42c8607e1a11a929fd5685d28
KHabustova <habustovakr@gmail.com>
2025-04-01 12:46:17 +0200

    - added .env file

--- src/main/java/dreamjournal/WebConfig.java
+++ src/main/java/dreamjournal/WebConfig.java
@@ -1,6 +1,7 @@
 
 package dreamjournal;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -16,13 +17,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     public WebMvcConfigurer webConfig(){
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 WebMvcConfigurer.super.addCorsMappings(registry);
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(frontendUrl)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
